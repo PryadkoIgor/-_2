44a726fb96da4c570f2b24c6b64da15cca166fbe
ROGI <impik@list.ru>
2022-04-21 15:53:11 +0300

    Задача_13

---
+++ Задача_13/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+Console.WriteLine("Введите число");
+int num1=Convert.ToInt32(Console.ReadLine());
+string numS=num1.ToString();
+if (num1>99)
+{
+    Console.WriteLine(numS[2]);
+}
+else
+Console.WriteLine ("Третьего числа нет");
---
+++ Задача_19/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+string num = Convert.ToString(new Random().Next(10000, 100000));
+bool pol = true;
+for (int i = 0; i < num.Length / 2; i++)
+{
+    if (num[i] != num[num.Length - i - 1])
+    {
+        pol = false;
+        break;
+    }
+}
+if (pol)
+{
+    Console.WriteLine("Число является полиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является полиндромом");
+}
